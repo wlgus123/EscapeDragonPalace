e88b15e9e16f4242dd3cacea7fbb67ed397f1f54
지현 <88700503+wlgus123@users.noreply.github.com>
2025-07-24 23:25:00 +0900

    맵
    - 기본 맵 크기 설정 및 출력
    - 용궁 맵 제작
    - 실행 시 맵 크기 변경
    - 프로그램 제목 변경

---
+++ EscapeDragonPalace/init.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// inlcude
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <conio.h>
+#include <stdbool.h>
+#include "mtg.h" // mtg 파일 불러오기
+
+// 메크로
+#define SCREEN_WIDTH 80 // 화면 너비
+#define SCREEN_HEIGHT 25 // 화면 높이
+#define STR_MAX 256 // 문자열 최대 크기
+
+// 구조체
+typedef struct MyPoint
+{
+    float x;
+    float y;
+} MyPoint;
---
+++ EscapeDragonPalace/main.c
@@ -0,0 +1,47 @@
+#include "init.h"
+#include "map.h"
+
+int g_Key;
+
+// 함수
+
+// 화면 그리기
+void Draw()
+{
+    DrawMap(); // 맵 그리기
+}
+
+// 업데이트 내용
+void Update()
+{
+    UpdateMapPos();
+}
+
+// 키 입력
+void Input()
+{
+    g_Key = _GetKey();
+}
+
+void main()
+{
+    // init
+    _BeginWindow();
+
+    // 맵 크기 변경
+    //col = 가로, lines = 세로
+    system("mode con:cols=78 lines=25");
+    SetConsoleTitle("용궁탈출");
+
+    //로직
+    while (true)
+    {
+        Input(); // 키 입력
+
+        Update(); // 업데이트
+
+        _Invalidate(); // 화면 그리기 (Draw() 함수 자동 적용)
+    }
+
+    _EndWindow();
+}
