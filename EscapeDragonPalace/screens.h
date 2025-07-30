b83760d6f6768acb82a6dcd7d4a14476864ffc31
지현 <88700503+wlgus123@users.noreply.github.com>
2025-07-30 14:41:34 +0900

    screen.h, screen.c에 Get 함수 생성

--- EscapeDragonPalace/screens.h
+++ EscapeDragonPalace/screens.h
@@ -4,9 +4,8 @@ void GameStartScreen();
 void GameOverScreen();
 void DrawStartScreen();
 void ReturnStartScreen();
-
-static bool GameStart = false;
-static bool GameStartText = true;
+bool GetGameStart(); // 게임 시작 여부 가져오는 함수
+bool GetGameStartText();
 
 static bool IsGameOver = false; // 게임 오버 여부
 static bool GameOverText = true;
