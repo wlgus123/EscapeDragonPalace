4fcfdb507b8abaa186ac03b8853ea60e050924b1
지현 <88700503+wlgus123@users.noreply.github.com>
2025-08-05 23:21:40 +0900

    조개 Draw 코드 원상태로 돌림

--- EscapeDragonPalace/clam.c
+++ EscapeDragonPalace/clam.c
@@ -26,31 +26,31 @@ void UpdateClam(MyPoint pos, bool alive) {
 }
 
 void DrawClam(MyPoint pos) {
-    int tempX = pos.x - GetPlusX();
+    int tempX = pos.x + GetPlusX();
+
+    //if (g_Clam.alive) {
+    //    for (int y = 0; y < CLAM_HEIGHT; y++) {
+    //        for (int x = 0; x < CLAM_WIDTH; x++)
+    //        {
+    //            // 몬스터 위치가 화면 내에 있을 때만 출력
+    //            if (tempX - x >= 0 && tempX + x < SCREEN_WIDTH)
+    //            {
+    //                _DrawText(tempX + x, pos.y + y, (char[]) { clamGraphic[x], 0 });
+    //            }
+    //        }
+    //    }
+    //}
+    //else
+    //{
+    //    _DrawText(pos.x + GetPlusX(), pos.y, " ");
+    //}
 
     if (g_Clam.alive) {
-        for (int y = 0; y < CLAM_HEIGHT; y++) {
-            for (int x = 0; x < CLAM_WIDTH; x++)
-            {
-                // 몬스터 위치가 화면 내에 있을 때만 출력
-                if (tempX + x >= 0 && tempX + x < SCREEN_WIDTH)
-                {
-                    _DrawText(tempX + x, pos.y + y, (char[]) { clamGraphic[x], 0 });
-                }
-            }
-        }
-    }
-    else
-    {
-        _DrawText(pos.x + GetPlusX(), pos.y, " ");
-    }
-
-    /*if (g_Clam.alive) {
         _DrawText(pos.x, pos.y, clamGraphic);
     }
     else {
         _DrawText(pos.x, pos.y, " ");
-    }*/
+    }
 }
 
 
