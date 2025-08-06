ff0da1f8b4b03beccaad0c4e83511d1a9cd1dabf
HaYoung1027 <rachel0393@naver.com>
2025-08-06 16:26:27 +0900

    item 수정

--- EscapeDragonPalace/item.c
+++ EscapeDragonPalace/item.c
@@ -65,10 +65,10 @@ void DrawItem(Item* item, int frame) {
 
 	// 아이템 타입에 따라 해초 or 공기방울 sprite 선택
 	switch (item->type) {
-	case ITEM_LIFE:
+	case E_ITEM_LIFE:
 		sprite = &seaweedSprite;
 		break;
-	case ITEM_SPEED:
+	case E_ITEM_SPEED:
 		sprite = &bubblesSprite;
 		break;
 	}
@@ -101,20 +101,18 @@ void InitItem() {
 
 	// 해초
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 353, .y = 14,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 0
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 530, .y = 12,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 0
@@ -122,20 +120,18 @@ void InitItem() {
 
 	// 공기방울
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 160, .y = 22,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 0
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 570, .y = 22,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 0
@@ -147,30 +143,27 @@ void InitItem() {
 
 	// 해초
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 206, .y = 9,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 1
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 288, .y = 6,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 1
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 467, .y = 1,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 1
@@ -178,30 +171,27 @@ void InitItem() {
 
 	// 공기방울
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 190, .y = 22,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 1
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 340, .y = 22,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 1
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 484, .y = 22,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 1
@@ -214,20 +204,18 @@ void InitItem() {
 
 	// 해초
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 99, .y = 21,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 2
 	};
 
 	itemList[numItem++] = (Item){
-	.type = ITEM_LIFE,
+	.type = E_ITEM_LIFE,
 	.x = 429, .y = 21,
 	.isHeld = false,
-	.value = 2,
 	.width = 11,
 	.height = 3,
 	.mapStatus = 2
@@ -235,40 +223,36 @@ void InitItem() {
 
 	// 공기방울
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 131, .y = 9,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 2
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 282, .y = 8,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 2
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 386, .y = 14,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 2
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 563, .y = 13,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 2
@@ -281,40 +265,36 @@ void InitItem() {
 
 	// 해초
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 100, .y = 21,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 3
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 210, .y = 5,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 3
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 471, .y = 21,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 3
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_LIFE,
+		.type = E_ITEM_LIFE,
 		.x = 542, .y = 11,
 		.isHeld = false,
-		.value = 2,
 		.width = 11,
 		.height = 3,
 		.mapStatus = 3
@@ -322,20 +302,18 @@ void InitItem() {
 
 	// 공기방울
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 138, .y = 11,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 3
 	};
 
 	itemList[numItem++] = (Item){
-		.type = ITEM_SPEED,
+		.type = E_ITEM_SPEED,
 		.x = 440, .y = 17,
 		.isHeld = true,
-		.value = 1,
 		.width = 5,
 		.height = 2,
 		.mapStatus = 3
